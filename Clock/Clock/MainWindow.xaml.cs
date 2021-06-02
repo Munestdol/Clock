8b979ced212c8b5cdd8caa6abcbeba8911c4abcb
Munestdol <munestdol@gmail.com>
2021-06-03 01:23:11 +0300

    Completed project

--- Clock/Clock/MainWindow.xaml.cs
+++ Clock/Clock/MainWindow.xaml.cs
@@ -23,6 +23,16 @@ namespace Clock
         public MainWindow()
         {
             InitializeComponent();
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+        }
+
+        private void CompositionTarget_Rendering(object sender, object args)
+        {
+            DateTime dt = DateTime.Now;
+            rotateSecond.Angle = 6 * (dt.Second + dt.Millisecond / 1000.0);
+            rotateMinute.Angle = 6 * dt.Minute + rotateSecond.Angle / 60;
+            rotateHour.Angle = 30 * (dt.Hour % 12) + rotateMinute.Angle / 12;
         }
     }
 }
